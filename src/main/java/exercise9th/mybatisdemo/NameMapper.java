d86cb4e065194f13de9fd3993f4b1363ed269d16
Suzuki Takayuki <tk.suzuki.0404@gmail.com>
2022-11-06 23:36:30 +0900

    feature/Read-api using MyBatis

---
+++ src/main/java/exercise9th/mybatisdemo/NameMapper.java
@@ -0,0 +1,15 @@
+package exercise9th.mybatisdemo;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+import java.util.Optional;
+
+@Mapper
+public interface NameMapper {
+
+    @Select("SELECT * FROM names")
+    List<Name> findAll();
+
+}
