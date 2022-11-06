bc7f78506151dc73a6ac4ed949007bbe0b72c81c
Suzuki Takayuki <tk.suzuki.0404@gmail.com>
2022-11-06 16:40:16 +0900

    Set up 9th Project

---
+++ src/main/java/exercise9th/mybatisdemo/MybatisdemoApplication.java
@@ -0,0 +1,13 @@
+package exercise9th.mybatisdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybatisdemoApplication.class, args);
+	}
+
+}
