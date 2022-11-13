dd25d8e6b48ad75d722a76375677aebd3ab7e37f
Suzuki Takayuki <tk.suzuki.0404@gmail.com>
2022-11-13 22:21:30 +0900

    Replace DI in Controller from Mapper to Service

--- src/main/java/exercise9th/mybatisdemo/NameController.java
+++ src/main/java/exercise9th/mybatisdemo/NameController.java
@@ -1,6 +1,7 @@
 package exercise9th.mybatisdemo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -8,16 +9,20 @@ import java.util.List;
 @RestController
 public class NameController {
 
-    private final NameMapper nameMapper;
+    private final NameService nameService;
 
-
-    public NameController(NameMapper nameMapper) {
-        this.nameMapper = nameMapper;
+    public NameController(NameService nameService) {
+        this.nameService = nameService;
     }
 
     @GetMapping("/names")
     public List<Name> getNames(){
-
-        return nameMapper.findAll();
+        return nameService.findAll();
     }
+
+    @GetMapping("/names/{id}")
+    public List<Name> getNameById(@PathVariable("id") int id) throws Exception {
+        return List.of(nameService.findById(id));
+    }
+
 }
