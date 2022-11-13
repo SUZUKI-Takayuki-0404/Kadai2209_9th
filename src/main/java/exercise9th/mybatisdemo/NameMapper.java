e10fad859df79f52d585f7c7677add32fd38d8da
Suzuki Takayuki <tk.suzuki.0404@gmail.com>
2022-11-13 22:43:10 +0900

    Add findById-method in NameMapper-class

--- src/main/java/exercise9th/mybatisdemo/NameMapper.java
+++ src/main/java/exercise9th/mybatisdemo/NameMapper.java
@@ -12,4 +12,7 @@ public interface NameMapper {
     @Select("SELECT * FROM names")
     List<Name> findAll();
 
+    @Select("SELECT * FROM names WHERE id = #{id}")
+    Optional<Name> findById(int id);
+
 }
