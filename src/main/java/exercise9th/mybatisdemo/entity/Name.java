db00b96ec23074d787278881ee59da3bb73bc276
Suzuki Takayuki <tk.suzuki.0404@gmail.com>
2022-11-19 11:09:11 +0900

    Update Name-class

--- src/main/java/exercise9th/mybatisdemo/entity/Name.java
+++ src/main/java/exercise9th/mybatisdemo/entity/Name.java
@@ -1,21 +1,13 @@
 package exercise9th.mybatisdemo.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
 public class Name {
 
     private int id;
     private String name;
-
-    public Name(int id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
 
 }
