1c9781073c527e296ee12f44485818a3e85b5a3c
Suzuki Takayuki <tk.suzuki.0404@gmail.com>
2022-11-19 11:02:14 +0900

    Update NameResponse-class

--- src/main/java/exercise9th/mybatisdemo/controller/NameResponse.java
+++ src/main/java/exercise9th/mybatisdemo/controller/NameResponse.java
@@ -1,9 +1,11 @@
 package exercise9th.mybatisdemo.controller;
 
 import exercise9th.mybatisdemo.entity.Name;
+import lombok.Data;
 
+@Data
 public class NameResponse {
-    
+
     private int id;
     private String name;
 
@@ -12,19 +14,4 @@ public class NameResponse {
         this.name = name.getName();
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
